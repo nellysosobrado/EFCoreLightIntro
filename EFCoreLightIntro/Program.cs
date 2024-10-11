5c5ffa5df2833d2324ae2cce49c585f320fc7448
Richard Chalk <barmybrit@gmail.com>
2024-10-11 18:48:00 +0200

    Add project files.

---
+++ EFCoreLightIntro/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreLightIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
